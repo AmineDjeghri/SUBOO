17cebf249bacd0535b51c10e83eae0ef0d8a7471
guillaume <guillaume.ridey94@gmail.com>
2019-12-12 16:12:07 +0100

    probleme avec setconstruire

--- src/entite/Entite.java
+++ src/entite/Entite.java
@@ -5,170 +5,67 @@ package entite;
 
 import itf.IEntite;
 import static itf.IUnite.*;
+
+import java.util.ArrayList;
 import java.util.Set;
 import itf.IUnite;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author sebma
-* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public class Entite implements IEntite {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	private Set<Entite> construit;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	private ArrayList<Entite> construit = new ArrayList<Entite>();
 	private IUnite nomUnite;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	private Integer tpsRestant;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	private EtatEntite etatEntite;
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#setDisponible()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public void setDisponible() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-
-		// end-user-code
+		etatEntite=EtatEntite.DISPONIBLE;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#setEnConstruction()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public void setEnConstruction() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-
-		// end-user-code
+		etatEntite=EtatEntite.ENCONSTRUCTION;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#setMobilise()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void setMobilise() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-
-		// end-user-code
+		etatEntite=EtatEntite.MOBILISE;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#setIdentite(IUnite unite)
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public void setIdentite(IUnite unite) {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-
-		// end-user-code
+		nomUnite = unite;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#isDisponible()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Boolean isDisponible() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-		return null;
-		// end-user-code
+		return (etatEntite == EtatEntite.DISPONIBLE);
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#isEnConstruction()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public Boolean isEnConstruction() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-		return null;
-		// end-user-code
+		return (etatEntite == EtatEntite.ENCONSTRUCTION);
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#isMobilise()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Boolean isMobilise() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-		return null;
-		// end-user-code
+		return (etatEntite == EtatEntite.MOBILISE);
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#setConstruit(IUnite... construit)
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
-	public void setConstruit(IUnite... construit) {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-
-		// end-user-code
-	}
-
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#setTempsRestant(Integer temps)
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void setTempsRestant(Integer temps) {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-
-		// end-user-code
+		tpsRestant = temps;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#getIdentite()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public IUnite getIdentite() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-		return null;
-		// end-user-code
+		return nomUnite;
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IEntite#getTempsRestant()
-	* @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+	
 	public Integer getTempsRestant() {
-		// begin-user-code
-		// TODO Module de remplacement de méthode auto-généré
-		return null;
-		// end-user-code
+		return tpsRestant;
+	}
+
+
+	@Override
+	public void setConstruit(IUnite... construit) {
+		// TODO Auto-generated method stub
+		
 	}
 }
